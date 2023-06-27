60ebb3db32c2de37f0d7e4e5ac943114bbb1f01d
mschijf <mschijf@gmail.com>
2023-06-27 17:40:31 +0200

    Day 1

--- src/main/kotlin/adventofcode2017/december01/PuzzleSolver.kt
+++ src/main/kotlin/adventofcode2017/december01/PuzzleSolver.kt
@@ -8,6 +8,15 @@ fun main() {
 
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
+    private val list = inputLines.first().map { it.digitToInt() }
+
+    override fun resultPartOne(): Any {
+        return list.filterIndexed { index, i -> i == list[(index+1) % list.size] }.sum()
+    }
+
+    override fun resultPartTwo(): Any {
+        return list.filterIndexed { index, i -> i == list[(index+list.size/2) % list.size] }.sum()
+    }
 }
 
 
