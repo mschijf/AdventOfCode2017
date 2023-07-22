99b71db2eb2cbccc01f65d4a1abdd40c15beca8c
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-07-22 15:40:51 +0200

    further refactoring useOf GridPosd, XYCoordinate, Point

--- src/main/kotlin/tool/coordinate/twodimensional/GridPos.kt
+++ src/main/kotlin/tool/coordinate/twodimensional/GridPos.kt
@@ -1,6 +1,6 @@
 package tool.coordinate.twodimensional
 
-fun posOf(x: Int, y: Int): Point = GridPos(x,y)
+fun posOf(x: Int, y: Int): Point = GridPos.of(x,y)
 
 fun posRange(minPos: Point, maxPos: Point) = sequence<Point> {
     (minPos.x .. maxPos.x).forEach { x ->
@@ -10,7 +10,7 @@ fun posRange(minPos: Point, maxPos: Point) = sequence<Point> {
     }
 }
 
-data class GridPos(override val x: Int, override val y: Int): Point(x, y) {
+data class GridPos private constructor(override val x: Int, override val y: Int): Point(x, y) {
 
     override fun above(other: Point) = this.y < other.y
     override val ONEDOWN = 1
@@ -18,8 +18,8 @@ data class GridPos(override val x: Int, override val y: Int): Point(x, y) {
     override fun plusXY(dx: Int, dy: Int) = posOf(x+dx, y+dy)
 
     companion object {
-        fun of(input: String) = XYPair.fromString(input).run { posOf(this.x, this.y) }
+        fun of(x: Int, y: Int): Point = GridPos(x,y)
+        fun of(input: String): Point = XYPair.fromString(input).run { GridPos(this.x, this.y) }
         val origin = posOf(0,0)
     }
 }
-
--- src/main/kotlin/tool/coordinate/twodimensional/XYCoordinate.kt
+++ src/main/kotlin/tool/coordinate/twodimensional/XYCoordinate.kt
@@ -1,8 +1,8 @@
 package tool.coordinate.twodimensional
 
-fun xyCoordinateOf(x: Int, y: Int): Point = XYCoordinate(x,y)
+fun xyCoordinateOf(x: Int, y: Int): Point = XYCoordinate.of(x,y)
 
-data class XYCoordinate(override val x: Int, override val y: Int): Point(x, y) {
+data class XYCoordinate private constructor(override val x: Int, override val y: Int): Point(x, y) {
 
     override fun above(other: Point) = this.y > other.y
     override val ONEDOWN = -1
@@ -10,7 +10,8 @@ data class XYCoordinate(override val x: Int, override val y: Int): Point(x, y) {
     override fun plusXY(dx: Int, dy: Int) = xyCoordinateOf(x+dx, y+dy)
 
     companion object {
-        fun of(input: String) = XYPair.fromString(input).run { xyCoordinateOf(this.x, this.y) }
+        fun of(x: Int, y: Int): Point = XYCoordinate(x,y)
+        fun of(input: String): Point  = XYPair.fromString(input).run { XYCoordinate(this.x, this.y) }
         val origin = xyCoordinateOf(0,0)
     }
 }
