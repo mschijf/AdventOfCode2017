dab35f069c885824d8f3452edc66c3377a46f2a6
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-07-23 08:09:53 +0200

    Day 23

---
+++ src/main/kotlin/adventofcode2017/december23/PuzzleSolver.kt
@@ -0,0 +1,125 @@
+package adventofcode2017.december23
+
+import adventofcode2017.PuzzleSolverAbstract
+import tool.math.getPrimeList
+import tool.math.isPrime
+
+fun main() {
+    PuzzleSolver(test=false).showResult()
+}
+
+class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
+
+    override fun resultPartOne(): Any {
+        val program = Program(inputLines, mutableListOf<Long>(0,0,0,0, 0,0,0,0))
+        return program.runProgram()
+    }
+
+    /**
+     * Ik heb er voor gekozen om het programma te analyseren en te herschijven naar een 3GL taal.
+     * Goed kijken hoe de loopjes lopen en dan de logica reverse te analyseren. Dan komen we uit op
+     * het volgende programma
+     *
+     *        var h = 0
+     *        for (b in 108400..125400 step 17) {
+     *            var f = false
+     *            for (d in 2 until b) {
+     *                for (e in 2 until b) {
+     *                    if (d*e == b) {
+     *                        f = true
+     *                    }
+     *                }
+     *            }
+     *            if (f)
+     *                h++
+     *        }
+     *        return h
+     *
+     * De twee binnenste lussen bekijken of er delers zijn van b, anders gezegd of b wel of niet priem is. Dat kan al herschreven worden:
+     *
+     *        var h = 0
+     *        for (b in 108400..125400 step 17) {
+     *            var f = false
+     *            for (d in 2 until b) {
+     *                if (b % d == 0) {
+     *                    f = true
+     *                    break
+     *                }
+     *            }
+     *            if (f)
+     *                h++
+     *        }
+     *        return h
+     *
+     *  Dit runt al binnen 200 ms en levert 903 als antwoord op. Gebruikmakend van de isPrime() hulpfunctie die ik
+     *  al had, kan het nog korter (maar niet sneller):
+     *
+     *        return (108400..125400 step 17).filterNot { it.isPrime() }.count()
+     *
+     */
+
+    override fun resultPartTwo(): Any {
+//        return (108400..125400 step 17).filterNot { it.isPrime() }.count()
+        var h = 0
+        for (b in 108400..125400 step 17) {
+            var f = false
+            for (d in 2 until b) {
+                if (b % d == 0) {
+                    f = true
+                    break
+                }
+            }
+            if (f)
+                h++
+        }
+        return h
+    }
+}
+
+
+
+class Program(
+    private val programList: List<String>,
+    private val register: MutableList<Long> ){
+
+    private var lastPlayedFrequency = -1L
+    private var ip = 0
+
+    private var countMuls = 0L
+
+    fun runProgram(): Long {
+        while(ip in programList.indices) {
+            executeStep()
+        }
+        return countMuls
+    }
+
+    private fun executeStep() {
+        val statement = programList[ip].toStatement()
+        val regName = if (statement.second.isRegister()) statement.second.asRegister()-'a' else -1
+        val firstValue = if (statement.second.isRegister()) register[statement.second.asRegister()-'a'] else statement.second.asLong()
+        val secondValue = if (statement.third.isRegister()) register[statement.third.asRegister()-'a'] else statement.third.asLong()
+        when (statement.instruction()) {
+            "set" -> {register[regName] = secondValue; ip++}
+            "sub" -> {register[regName] -= secondValue; ip++}
+            "mul" -> {register[regName] *= secondValue; countMuls++; ip++}
+            "jnz" -> if (firstValue != 0L) ip += secondValue.toInt() else ip++
+            else -> throw Exception("unknown instruction")
+        }
+    }
+
+    private fun String.toStatement(): Triple<String, String, String> {
+        val words = this.trim().split(" ")
+        return if (words.size == 3) {
+            Triple(words[0].trim(), words[1].trim(), words[2].trim())
+        } else {
+            Triple(words[0].trim(), words[1].trim(), "0")
+        }
+    }
+
+    private fun Triple<String, String, String>.instruction() = this.first
+    private fun String.isRegister() = (this.length == 1) && (this[0] in 'a' .. 'z')
+    private fun String.asRegister() = this[0]
+    private fun String.asLong() = this.toLong()
+}
+
